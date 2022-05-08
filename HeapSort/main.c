b4f11d6388004e46f6bd9d6a1cb05a9b13a3f3ba
Luiz Fernando Seibel Carrilho <43753872+Lfseibel@users.noreply.github.com>
2022-05-08 09:16:11 -0400

    Update main.c

--- HeapSort/main.c
+++ HeapSort/main.c
@@ -3,62 +3,63 @@
 #include <stdlib.h>
 #include "heap.h"
 
-  int main() {
+int main() 
+{
 
-	char nomeArquivo[30];
-	int tamanho = 100;
-	int *vet;
-	double time_spent = 0.0;
+  char nomeArquivo[30];
+  int tamanho = 100;
+  int *vet;
+  double time_spent = 0.0;
   int contadorTroca = 0;
   int contadorInteracao = 0;
 
-	printf("Digite o nome do arquivo: \n");
-	scanf("%s", nomeArquivo);
+  printf("Digite o nome do arquivo: \n");
+  scanf("%s", nomeArquivo);
 
 ////////////////////////////////////////////////////////////////	
-	geraAleatorios(nomeArquivo, tamanho); 
+  geraAleatorios(nomeArquivo, tamanho); 
   vet = leArquivo(nomeArquivo, tamanho);
 		
-	clock_t begin = clock();
+  clock_t begin = clock();
   heapSort(vet, tamanho, &contadorTroca, &contadorInteracao);
   clock_t end = clock();	
 
-	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+  time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
 		
-	resultadoHeapS ("Aleatório", contadorTroca, contadorInteracao, tamanho, time_spent);
+  resultadoHeapS ("Aleatório", contadorTroca, contadorInteracao, tamanho, time_spent);
     
-	free(vet);
+  free(vet);
 
 ////////////////////////////////////////////////////////////////
-	contadorTroca = 0;
+  contadorTroca = 0;
   contadorInteracao = 0;
-	geraCrescente(nomeArquivo, tamanho); 
+  geraCrescente(nomeArquivo, tamanho); 
   vet = leArquivo(nomeArquivo, tamanho);
 	
-	time_spent = 0.0;	
-	begin = clock();
+  time_spent = 0.0;	
+  begin = clock();
   heapSort(vet, tamanho, &contadorTroca, &contadorInteracao);
   end = clock();
 
-	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+  time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
 		
-	resultadoHeapS ("Crescente", contadorTroca, contadorInteracao, tamanho, time_spent);
+  resultadoHeapS ("Crescente", contadorTroca, contadorInteracao, tamanho, time_spent);
 
-	free(vet);
-	////////////////////////////////////////////////////////////////
-	contadorTroca = 0;
+  free(vet);
+////////////////////////////////////////////////////////////////
+  contadorTroca = 0;
   contadorInteracao = 0;
-	geraDecrescente(nomeArquivo, tamanho); 
+  geraDecrescente(nomeArquivo, tamanho); 
   vet = leArquivo(nomeArquivo, tamanho);
 	
-	time_spent = 0.0;	
-	begin = clock();
+  time_spent = 0.0;	
+  begin = clock();
   heapSort(vet, tamanho, &contadorTroca, &contadorInteracao);
   end = clock();
 
-	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+  time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
 		
-	resultadoHeapS ("Decrescente", contadorTroca, contadorInteracao, tamanho, time_spent);
+  resultadoHeapS ("Decrescente", contadorTroca, contadorInteracao, tamanho, time_spent);
 
-	free(vet);
-  }
+  free(vet);
+ }
