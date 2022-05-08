cbf486136753eb23135bbe9b14f2833463b958be
Luiz Fernando Seibel Carrilho <43753872+Lfseibel@users.noreply.github.com>
2022-05-08 09:17:14 -0400

    Update main.c

--- MergeSort/main.c
+++ MergeSort/main.c
@@ -3,11 +3,12 @@
 #include <time.h>
 #include <stdlib.h>
 
-int main(void) {
+int main(void) 
+{
   char nomeArquivo[30];
   int tamanho = 100;
   int *vet;
-	double time_spent = 0.0;
+  double time_spent = 0.0;
   int contadorTroca = 0;
   int contadorInteracao = 0;
 	
@@ -22,27 +23,27 @@ int main(void) {
   mergeSort(vet, 0, tamanho-1, &contadorTroca, &contadorInteracao); 
   clock_t end = clock();
   
-	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+  time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
   
-	resultadoMergeS ("Aleatório", contadorTroca, contadorInteracao, tamanho, time_spent);
+  resultadoMergeS ("Aleatório", contadorTroca, contadorInteracao, tamanho, time_spent);
   
-	free(vet);
+  free(vet);
 ////////////////////////////////////////////////////////////// 
   contadorTroca = 0;
   contadorInteracao = 0;
   geraCrescente(nomeArquivo, tamanho);
   vet = leArquivo(nomeArquivo, tamanho);
   
-	time_spent = 0.0;
+  time_spent = 0.0;
   
-	begin = clock();
+  begin = clock();
   mergeSort(vet, 0, tamanho-1, &contadorTroca, &contadorInteracao);
   end = clock();
   
-	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+  time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
   resultadoMergeS ("Crescente", contadorTroca, contadorInteracao, tamanho, time_spent);
   
-	free(vet);
+  free(vet);
 
 //////////////////////////////////////////////////////////////
   contadorTroca = 0;
@@ -50,17 +51,17 @@ int main(void) {
   geraDecrescente(nomeArquivo, tamanho);
   vet = leArquivo(nomeArquivo, tamanho);
   
-	time_spent = 0.0;
+  time_spent = 0.0;
   
-	begin = clock();
+  begin = clock();
   mergeSort(vet, 0, tamanho-1, &contadorTroca, &contadorInteracao);
   end = clock();
   
-	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+  time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
   
-	resultadoMergeS ("Decrescente", contadorTroca, contadorInteracao, tamanho, time_spent);
+  resultadoMergeS ("Decrescente", contadorTroca, contadorInteracao, tamanho, time_spent);
   
-	free(vet);
+  free(vet);
 
  
   return 0;
